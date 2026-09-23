f6adbda8f5c63fa9fd97949c5245881020792840
Student8000 <wi26b041@technikum-wien.at>
2026-09-23 20:40:53 +0200

    Import readData module in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const crypto = require("crypto");
-
+const readData = require("./readData");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
