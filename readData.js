3ae8a7b06680691fba932d69cc2b8758a8770eae
Student8000 <wi26b041@technikum-wien.at>
2026-09-23 20:36:41 +0200

    Add readData function to read JSON data from file

--- readData.js
+++ readData.js
@@ -1,1 +1,11 @@
+const fs = require("fs");
+const path = require("path");
 
+function readData() {
+    const filePath = path.join(__dirname, "quizData.json");
+    const data = fs.readFileSync(filePath, "utf8");
+
+    return JSON.parse(data);
+}
+
+module.exports = readData;
